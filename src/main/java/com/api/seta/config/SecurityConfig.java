c6836fc0e437715c98e7718b37b5a6cbcc717746
Gabriel Pimentel <dev.gabrielpimentel@gmail.com>
2025-05-11 15:32:09 -0300

    Up: including localhost mobile front end address to allowed origins

--- src/main/java/com/api/seta/config/SecurityConfig.java
+++ src/main/java/com/api/seta/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3030"));
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3030", "http://localhost:8081"));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
     configuration.setAllowCredentials(true);
